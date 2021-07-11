84a767d222bd65fc3b6c2741153e99a6b1c9e230
Thomas Offergeld <t.offergeld@iaew.rwth-aachen.de>
2021-07-11 16:06:45 +0200

    Update with layouts and search form

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import cimpyorm as cpo
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-schema = cpo.Schema()
 templates = Jinja2Templates(directory="templates")
 
+schema = cpo.Schema()
+
+cnames = schema.model.classes.__dict__
+enumnames = schema.model.enum.__dict__
+dtypenames = schema.model.dt.__dict__
+typemap = \
+    {**{name: "Class" for name in cnames.keys()},
+    **{name: "Enumeration" for name in enumnames.keys()},
+    **{name: "Datatype" for name in dtypenames.keys()},}
+model = {**cnames, **enumnames, **dtypenames}
 
 @app.get("/")
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("layout.html", {"request": request})
 
 
 @app.get("/class/{name}")
@@ -21,16 +30,6 @@ def class_(request: Request, name: str):
     return templates.TemplateResponse("classes.html", 
     {"request": request, "table": class_.to_html(
         classes=("table", "table-striped"))})
-
-
-@app.get("/classes")
-def all_classes():
-    return [
-        {
-            "name": c.name,
-            "namespace": c.namespace.short
-        }
-        for c in schema.class_hierarchy()]
 
 
 @app.get("/classes/{classname}")
@@ -63,6 +62,47 @@ def hierarchy(start: str):
     start = vars(schema.model.classes)[start]
     res = _recurse_hierarchy(start)
     return res
+
+
+
+@app.get("/show_table/{objectname}")
+def show_table(request: Request, objectname: str):
+    if objectname in model.keys():
+        return templates.TemplateResponse("table.html", 
+        {
+            "request": request, 
+            "objectname": objectname, 
+            "category": typemap[objectname],
+            "table": model[objectname].to_html(
+                index=False, 
+                classes=["table", "table-striped", "table-hover"], 
+                table_id="description")
+        }
+        )
+
+
+@app.get("/show_table")
+def show_table_empty(request: Request):
+    pass
+
+
+@app.get("/classes")
+def classes(request: Request):
+    return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(cnames.keys())})
+
+@app.get("/enums")
+def enums(request: Request):
+    return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(enumnames.keys())})
+
+@app.get("/dtypes")
+def dtypes(request: Request):
+    return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(dtypenames.keys())})
+
+
+@app.get("/objectnames")
+def objectnames():
+    return list(model.keys())
+
 
 
 def _recurse_hierarchy(node):
