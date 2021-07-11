084d9a25e75d08346d4db76ab499c7c414381e06
Thomas Offergeld <t.offergeld@iaew.rwth-aachen.de>
2021-07-11 23:33:11 +0200

    Update path structure and exclude frontend routes from automatic documentation

--- main.py
+++ main.py
@@ -16,45 +16,33 @@ typemap = \
     **{name: "Datatype" for name in dtypenames.keys()},}
 model = {**cnames, **enumnames, **dtypenames}
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def index(request: Request):
     return templates.TemplateResponse("layout.html", {"request": request})
 
 
-@app.get("/classes/{classname}")
-async def get_class(classname: str):
-    """
-    Returns the properties of a class.
-
-    :param classname: The class to return
-    """
-    try:
-        c = vars(schema.model)[classname]
-        sc = c._schema_class
-    except KeyError:
-        return "No such element"
-
-    return {
-        "name": sc.name,
-        "parent": sc.parent.name if sc.parent is not None else None,
-        "properties": list(sc.all_props.keys())
-    }
-
-
-@app.get("/hierarchy")
+@app.get("/api/hierarchy")
 def default_hierarchy():
+    """
+    Return a class hierarchy of the schema, rooted at IdentifiedObject
+    """
     return hierarchy("IdentifiedObject")
 
 
-@app.get("/hierarchy/{start}")
+@app.get("/api/hierarchy/{start}")
 def hierarchy(start: str):
+    """
+    Return a class hierarchy of the schema, rooted at :start:
+
+    :param start: The class used for the hierarchy root.
+    """
     start = vars(schema.model.classes)[start]
     res = _recurse_hierarchy(start)
     return res
 
 
 
-@app.get("/details/{objectname}")
+@app.get("/details/{objectname}", include_in_schema=False)
 def details(request: Request, objectname: str):
     if objectname in model.keys():
         try: 
@@ -78,26 +66,29 @@ def details(request: Request, objectname: str):
             )
 
 
-@app.get("/show_table")
+@app.get("/show_table", include_in_schema=False)
 def show_table_empty(request: Request):
     pass
 
 
-@app.get("/classes")
+@app.get("/classes", include_in_schema=False)
 def classes(request: Request):
     return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(cnames.keys())})
 
-@app.get("/enums")
+@app.get("/enums", include_in_schema=False)
 def enums(request: Request):
     return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(enumnames.keys())})
 
-@app.get("/dtypes")
+@app.get("/dtypes", include_in_schema=False)
 def dtypes(request: Request):
     return templates.TemplateResponse("objects.html", {"request": request, "cnames": sorted(dtypenames.keys())})
 
 
-@app.get("/objectnames")
+@app.get("/api/objectnames")
 def objectnames():
+    """
+    Return all object names (classes, enumerations, datatypes) in the schema.
+    """
     return list(model.keys())
 
 
